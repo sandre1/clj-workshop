bc8449587f24645d016a218eef6f9184f680b038
Andrei Stan <andrei.stan@netdava.com>
2022-04-06 16:40:28 +0300

    activity 101 finish

--- src/sandre1/activity101.clj
+++ src/sandre1/activity101.clj
@@ -1,6 +1,17 @@
-(ns sandre1.activity101)
+(ns sandre1.activity101
+  (:use [clojure.repl]))
 
+(def base-co2 382)
 
+(def base-year 2006)
 
+(defn co2-estimate 
+ "Function that takes one integer parameter called year and
+returns the estimated level of CO2 ppm for that year."  
+  [year]
+  (let [year-diff (- year base-year)
+        estimate (+ base-co2 (* 2 year-diff))]
+    estimate))
 (comment 
-  )
+  (co2-estimate 2020)
+  (doc co2-estimate))
